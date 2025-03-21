edb69fbdb7b691ed35e835ba9ed6d2499090c6ba
Vidhyotha <vidhyotha@gmail.com>
2025-03-21 12:18:11 +0530

    minor changes

--- predict_vessel.sh
+++ predict_vessel.sh
@@ -12,9 +12,9 @@ python predict.py --model swinunetr --task vessel --device cuda:1 --model_path w
 python predict.py --model swinunetr --task vessel --device cuda:1 --model_path weights/swinunetr_fives_train,drive,chasedb1,stare,hrf,fives_test_latest/best_swinunetr_model.pth --datasets hei-med
 python predict.py --model swinunetr --task vessel --device cuda:1 --model_path weights/swinunetr_fives_train,drive,chasedb1,stare,hrf,hei-med_latest/best_swinunetr_model.pth --datasets fives_test
 
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,chasedb1,stare,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets drive
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,stare,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets chasedb1
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets stare
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hei-med,fives_test_latest/best_unetr_model.pth --datasets hrf
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hrf,fives_test_latest/best_unetr_model.pth --datasets hei-med
-# python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hrf,hei-med_latest/best_unetr_model.pth --datasets fives_test
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,chasedb1,stare,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets drive
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,stare,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets chasedb1
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,hrf,hei-med,fives_test_latest/best_unetr_model.pth --datasets stare
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hei-med,fives_test_latest/best_unetr_model.pth --datasets hrf
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hrf,fives_test_latest/best_unetr_model.pth --datasets hei-med
+python predict.py --model unetr --task vessel --device cuda:1 --model_path weights/unetr_fives_train,drive,chasedb1,stare,hrf,hei-med_latest/best_unetr_model.pth --datasets fives_test
